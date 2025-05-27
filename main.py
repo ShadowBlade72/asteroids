19a37d3aab640709988c8ef51d453de79a7ff9a0
ShadowBlade72 <UnderCoverSPI101@gmail.com>
2025-05-26 20:24:39 -0400

    Update FPS

--- main.py
+++ main.py
@@ -8,13 +8,19 @@ def main():
     print(f"Screen height: {SCREEN_HEIGHT}")
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 
+# FPSClock
+    clock = pygame.time.Clock()
+    dt = 0 #delta time
+
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
 
-        screen.fill("black")
-        pygame.display.flip()
+        screen.fill("black") # Fill the screen with black
+        pygame.display.flip() # Update the screen
+
+        dt = clock.tick(60) / 1000 # Limit the frame rate to 60 FPS
 
 if __name__ == "__main__":
     main()
